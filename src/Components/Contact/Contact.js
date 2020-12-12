8fc2ec8ee1045a9139697553542b3e16773a6032
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-12-12 22:33:19 +0530

    adjusting syles, changing meta data and updating manifest file

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
 
 function Contact() {
   return (
-     <div id="contact">
-       <h3 className="title">Contact<hr /></h3>
-       <p>Feel free to contact me on my social media.</p>
-        <div className="social-media">
-        <a href="https://instagram.com"><i className="fab fa-instagram"></i></a>
-        <a href="https://twitter.com"><i className='fab fa-twitter'></i></a>
-        <a href="https://github.com/Hardik5862"><i className='fab fa-github'></i></a>
-        <a href="https://linkedin.com/in/hardik-sachan-b0ab7b1a2"><i className='fab fa-linkedin-in'></i></a>
-        </div>
+    <div id="contact">
+      <h3 className="title">Contact</h3>
+      <p>Feel free to contact me on my social media.</p>
+      <div className="social-media">
+        <a href="https://instagram.com">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="https://twitter.com">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://github.com/Hardik5862">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://linkedin.com/in/hardik-sachan-b0ab7b1a2">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
       </div>
+    </div>
   );
 }
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import logo from "./logo.png";
 import "./navbar.css";
 
 function NavBar() {
   return (
     <Navbar collapseOnSelect expand="md" bg="light" variant="light" fixed="top">
       <Navbar.Brand className="logo">
-        <img src={logo} alt="Logo" className="img" />
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/myimage.jpg`}
+          alt="Logo"
+          className="img"
+        />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
