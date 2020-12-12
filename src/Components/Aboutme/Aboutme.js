ec4da2054a644cbf4509861ade43023ac08e2be8
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-12-12 23:59:20 +0530

    fixing errors in styling

--- src/Components/Aboutme/Aboutme.js
+++ src/Components/Aboutme/Aboutme.js
@@ -4,25 +4,27 @@ import Container from "react-bootstrap/Container";
 
 function Aboutme() {
   return (
-    <Container id="about">
+    <div id="about">
       <h3 className="title">About me</h3>
-      <div className="abt">
-        <img
-          alt="this is my pic"
-          src={`${process.env.PUBLIC_URL}/assets/myimage.jpg`}
-        />
-        <p>
-          Given n Magnets which are placed linearly, with each magnet to be
-          considered as of point object. Each magnet suffers force from its left
-          sided magnets such that they repel it to the right and vice versa. All
-          forces are repulsive. The force being equal to the distance (1/d , d
-          being the distance). Now given the positions of the magnets, the task
-          to find all the points along the linear line where net force is ZERO.
-          Note: Distance have to be calculated with precision of
-          0.0000000000001.
-        </p>
-      </div>
-    </Container>
+      <Container>
+        <div className="abt">
+          <img
+            alt="this is my pic"
+            src={`${process.env.PUBLIC_URL}/assets/myimage.jpg`}
+          />
+          <p>
+            Given n Magnets which are placed linearly, with each magnet to be
+            considered as of point object. Each magnet suffers force from its
+            left sided magnets such that they repel it to the right and vice
+            versa. All forces are repulsive. The force being equal to the
+            distance (1/d , d being the distance). Now given the positions of
+            the magnets, the task to find all the points along the linear line
+            where net force is ZERO. Note: Distance have to be calculated with
+            precision of 0.0000000000001.
+          </p>
+        </div>
+      </Container>
+    </div>
   );
 }
 
