ef892f6ab299c037c76d696f29bb1634c7e695cb
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-12-13 16:24:15 +0530

    updating home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+
+import "./Home.css";
 
 function Home() {
   return (
-  	<div id="home" className='bg'>
-  		<p>
-	  		Hi, I'm Hardik Sachan.
-	  	</p>
-  	</div>
+    <div id="home" className="bg">
+      <ReactTypingEffect
+        text={[
+          "Hi, I'm Hardik.",
+          "A web developer and,",
+          " a graphic designer.",
+        ]}
+        cursor="_"
+        speed="100"
+        eraseSpeed="100"
+        eraseDelay="1000"
+        typingDelay="1000"
+      />
+    </div>
   );
 }
 
